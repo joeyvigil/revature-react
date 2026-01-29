4c5515bf36ebe72beab59939a74eee552f775174
Joseph Vigil <joeyvigil109329@gmail.com>
2026-01-29 08:24:44 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // comment 
+
+  return (
+    <>
+      <h1>Revature + React</h1>
+      <p>We are writing in tsx </p>
+
+      {/* comment */}
+
+    </>
+  )
+}
+
+export default App
